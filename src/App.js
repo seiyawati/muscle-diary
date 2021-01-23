5cda30eb2d57a3b0dfee9feb5d549140237911ab
delta-EGG <76396661+delta-EGG@users.noreply.github.com>
2021-01-23 18:57:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-// import Calender from './pages/Calender'
-import Test from './pages/test'
+import { BrowserRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom'
+import { Calender, NotFound } from './pages'
 
-const App = () => <Test />
+const Home = () => (
+  <article>
+    <h1>
+      <Link to="/">Home</Link>
+    </h1>
+    <Link to="/calender">this is calender</Link>
+  </article>
+)
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={withRouter(Home)} />
+      <Route exact path="/calender" component={Calender} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+)
 export default App
