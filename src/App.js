54969e87ffb0e322e2a818b8100c8ac22c7e9c86
Seiya Kawamoto <kid960805@gmail.com>
2021-01-23 19:12:11 +0900

    :sparkles: ログインページレイアウト #11 #12

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom'
-import { Calender, NotFound } from './pages'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Calender, NotFound, Home } from './pages'
 
-const Home = () => (
-  <article>
-    <h1>
-      <Link to="/">Home</Link>
-    </h1>
-    <Link to="/calender">this is calender</Link>
-  </article>
-)
+// const Home = () => (
+//   <article>
+//     <h1>
+//       <Link to="/">Home</Link>
+//     </h1>
+//     <Link to="/calender">this is calender</Link>
+//   </article>
+// )
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={withRouter(Home)} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/calender" component={Calender} />
       <Route component={NotFound} />
     </Switch>
