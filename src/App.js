ce28dd6dc68433f76fda6f0ee647174012a2b66c
SasakiPeter <anagentofthegod@gmail.com>
2021-01-24 02:02:06 +0900

    :bug: ルーティング修正 #24

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Calender, NotFound, Home } from './pages'
 
+const Auth = (props) => <div>{props.children}</div>
+
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/calender" component={Calender} />
-      <Route component={NotFound} />
+      <Route exact path="/signin" component={Home} />
+      <Auth>
+        <Switch>
+          <Route exact path="/" component={Calender} />
+          <Route component={NotFound} />
+        </Switch>
+      </Auth>
     </Switch>
   </Router>
 )
