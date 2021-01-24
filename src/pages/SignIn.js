3dd5051d59b907b442764227d270096b597f23f1
Seiya Kawamoto <kid960805@gmail.com>
2021-01-24 12:40:45 +0900

    Feat issue48

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -16,13 +16,14 @@ import {
 } from 'react-social-login-buttons'
 import { makeStyles } from '@material-ui/core/styles'
 import Form from '../components/Atoms/Form'
+import Image from '../images/Muscle_Diary.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://star-labo.co.jp/wp-content/uploads/2020/06/topics-57.jpg)',
+    backgroundImage: `url(${Image})`,
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
@@ -38,13 +39,6 @@ const useStyles = makeStyles((theme) => ({
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
   },
 }))
 
